4642612fac47b42d5596dbcb5a93e0dcd9601261
NastyaSz <77804568+NastyaSz@users.noreply.github.com>
2021-03-14 15:43:40 +0200

    Added cw task 1-10

---
+++ Lesson 2/classwork/js/task3.js
@@ -0,0 +1,12 @@
+/*3. Перепишите с тернарным оператором
+            if (a > b) {
+                max = a;
+            } else {
+                max = b;
+            }
+            
+            console.log(max);*/
+
+ var a = 20;
+ var b = 12;           
+ var result = (a > b)? console.log(a): console.log(b);
