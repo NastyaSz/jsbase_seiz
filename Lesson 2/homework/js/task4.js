91b0379be8811f27e0b84208dbdd2bef099f5d3e
NastyaSz <77804568+NastyaSz@users.noreply.github.com>
2021-03-14 17:27:01 +0200

    Added HW2

---
+++ Lesson 2/homework/js/task4.js
@@ -0,0 +1,18 @@
+/* 4. Дано два числа A и B где (A < B). 
+    Выведите на экран суму всех чисел расположенных в числовом промежутке от А до В. 
+    Выведите на экран все нечетные значения, расположенные в числовом промежутке от А до В. */
+
+let minNumber = +prompt("Введите меньшее число");
+let maxNumber = +prompt("Введите большее число");
+let sum = 0;
+if(minNumber > maxNumber){
+    alert("Ошибка");
+}
+for(sum; minNumber <= maxNumber; minNumber++){
+    sum = sum + minNumber;
+    if(minNumber % 2 == 1){
+        document.write(minNumber + "<br>");
+    }
+
+}
+document.write(sum);
