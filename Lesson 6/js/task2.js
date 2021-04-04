93d54eea349041c9c3deb80f3afc15423b17eb4b
NastyaSz <77804568+NastyaSz@users.noreply.github.com>
2021-04-04 21:34:49 +0300

    Added HW6

---
+++ Lesson 6/js/task2.js
@@ -0,0 +1,53 @@
+/*2. Допишите функцию countOnline
+
+        let users = [
+            {
+                age: 27,
+                online: false
+            },
+            {
+                age: 32,
+                online: true
+            },
+            {
+                age: 48,
+                online: false
+            },
+            {
+                age: 19,
+                online: true
+            }
+        ];
+
+        function countOnline(arr) {}
+        console.log(countOnline(users)); */
+
+let users = [
+    {
+        age: 27,
+        online: false
+    },
+    {
+        age: 32,
+        online: true
+    },
+    {
+        age: 48,
+        online: false
+    },
+    {
+        age: 19,
+        online: true
+    }
+];
+function countOnline(arr) {
+    let count = 0; 
+    for (let prop in arr){
+        if (arr[prop].online == true){
+            count++;
+        }   
+    }
+    return count;
+   
+}
+console.log(countOnline(users));
